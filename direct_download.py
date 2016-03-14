47ddc4a877bf8f2d1a812dacb034e99f76a50604
Shubam Mehta <s26mehta@gmail.com>
2016-03-14 18:08:21 -0400

    Adding config file to hold global variables, credentials and logger

---
+++ direct_download.py
@@ -0,0 +1,8 @@
+"""
+Name: Google Cloud Storage Download
+
+Purpose: Direct Download Files in any browser from a google cloud storage bucket
+
+Note: For files to be downloadable from a browser, they will need to be to public or the generated urls must have an
+      expiry
+"""
