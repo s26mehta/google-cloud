1c8fb1a1638a608197e5da966506542d00bab1d9
Shubam Mehta <s26mehta@gmail.com>
2016-03-16 18:30:48 -0400

    Adding vm manager file

---
+++ vm_manager.py
@@ -0,0 +1,24 @@
+"""
+Name: VM manager
+
+Date Created: January 25, 2016
+
+Created By:
+    Shubam Mehta
+
+Purpose: Create, start, terminate and delete google cloud virtual machines via this script
+
+"""
+
+"""
+    Installing python on vm:
+    mkdir test
+    cd test/
+    wget https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz
+    tar -xvf Python-3.5.1.tgz
+    cd Python-3.5.1/
+    apt-get install build-essential
+    ./configure
+    make install
+"""
+
